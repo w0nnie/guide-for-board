678e2460067b8879c313036e7f438e7998f215f8
w0nnie <kjw55523@gmail.com>
2023-11-30 08:49:37 +0900

    #8 스프링부트 프로젝트 init
    * 액츄에이터
    * 롬복
    * 데브툴즈
    * 스프링

---
+++ src/test/java/com/guide/board/GuideForBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideForBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
