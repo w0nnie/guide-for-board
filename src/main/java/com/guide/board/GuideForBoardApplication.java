dec4b790d40f87248a3d562cb7cab99c0aa61f7f
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-11-30 10:03:41 +0900

    #8 스프링부트 프로젝트 init
    * 액츄에이터
    * 롬복
    * 데브툴즈
    * 스프링

---
+++ src/main/java/com/guide/board/GuideForBoardApplication.java
@@ -0,0 +1,13 @@
+package com.guide.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideForBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideForBoardApplication.class, args);
+    }
+
+}
