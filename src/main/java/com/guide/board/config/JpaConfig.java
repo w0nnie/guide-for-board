eda143044819b82384b9ce6d0980614131206b6a
w0nnie <kjw55523@gmail.com>
2023-12-01 17:23:04 +0900

    #14 Article domain

---
+++ src/main/java/com/guide/board/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.guide.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("w0nnie"); //todo: 스프링 시큐리티로 인증 기능 붙일떄, 수정
+    }
+}
--- src/main/java/com/guide/board/domain/Article.java
+++ src/main/java/com/guide/board/domain/Article.java
@@ -1,18 +1,79 @@
 package com.guide.board.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy")
+})
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동채번
     private Long id;
+
+    @Setter // setter는 각필드에 걸어주고싶다 class단위로 걸지않음
+    @Column(nullable = false)
     private String title; // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content; // 본문
+
+    @Setter
     private String hashtag;  // 해쉬태그
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
 
 
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    //영속화 되지 않는 값은 동등성 검사 탈락
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
