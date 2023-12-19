783b6856d7d60ae99046e6726037cb5ce41d9997
w0nnie <kjw55523@gmail.com>
2023-12-19 16:36:30 +0900

    #1 - data rest 테스트
    간단한 테스트 작성
    통합 테스트로 접근
    
    DB 접근이 일어나므로 안전을 위해 transactional 적용
    테스트 안에서는 롤백 정책 동작

---
+++ src/test/java/com/guide/board/controller/DataRestTest.java
@@ -0,0 +1,70 @@
+package com.guide.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mockMvc;
+
+    public DataRestTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("게시글 리스트 조회")
+    @Test
+    void search_article_list() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("게시글 단건 조회")
+    @Test
+    void search_article() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("게시글 댓글 리스트 조회")
+    @Test
+    void search_article_comments_list() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(get("/api/articles/2/articleComment"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("댓글 리스트 조회")
+    @Test
+    void search_article_comment_list() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(get("/api/articleComments/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
