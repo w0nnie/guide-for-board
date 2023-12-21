897dddbc759ee71df7117e4441248987ba7f5687
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-12-21 14:44:00 +0900

    게시판 api 만들기 - 구현까지 완료 (#19)
    * #1 - Spring Date Rest + HAL Explorer 디펜던시 추가
    
    API 간편하게 만들기위해 spring boot 기능 추가
    
    * #1 - data rest 설정
    
    게시글, 댓글의 json api 를 자동으로 restful 하게 만들게끔 설
    
    * #1 - data rest 테스트
    
    간단한 테스트 작성
    통합 테스트로 접근
    
    DB 접근이 일어나므로 안전을 위해 transactional 적용
    테스트 안에서는 롤백 정책 동작
    
    * #1 - 리팩토링: 게시글 repository EOF 추가
    
    * #1 - Spring Data REST 테스트 제외 처리
    
    전체 테스트 중 실행되지 않게 제외처리
    
    * #1 - Querydsl 디펜던시 추가
    
    Querydsl 환경구성
    자동생성 Q클래스 gitingore 추
    
    * #1 - Querydsl 이용하여 api 검색 옵션 추가
    
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 이용하여 개

--- src/main/java/com/guide/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/guide/board/repository/ArticleCommentRepository.java
@@ -1,9 +1,31 @@
 package com.guide.board.repository;
 
 import com.guide.board.domain.ArticleComment;
+import com.guide.board.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+
+        bindings.excludeUnlistedProperties(true);
+
+        bindings.including(root.content, root.createAt, root.createBy);
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/com/guide/board/repository/ArticleRepository.java
+++ src/main/java/com/guide/board/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.guide.board.repository;
 
 import com.guide.board.domain.Article;
+import com.guide.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+
+        bindings.including(root.title, root.content, root.hashtag, root.createAt, root.createBy);
+
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+
+    };
 }
