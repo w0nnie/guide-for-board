9fbc69022cf6b8ffe75345b33624d7d846aceafe
w0nnie <kjw55523@gmail.com>
2023-12-27 16:46:29 +0900

    #20 -게시글 뷰 테스트 업데이트
    기능 구현 중인 유닛테스트 `@Disabled`
    테스트 방법 추가

--- src/test/java/com/guide/board/controller/ArticleControllerTest.java
+++ src/test/java/com/guide/board/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.guide.board.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
+    @Disabled("구현중")
     @DisplayName("[GET] 게시글 리스트 페이지 - 정상 호출")
     @Test
     void getArticleListViewSuccess() throws Exception {
@@ -30,9 +32,11 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("article"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void getArticleDetailsViewSuccess() throws Exception {
@@ -42,9 +46,12 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     void getArticleSearchViewSuccess() throws Exception {
@@ -54,9 +61,10 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("articles/search"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[GET] 게시글 해쉬태그 검색 페이지 - 정상 호출")
     @Test
     void getArticleHashtagSearchViewSuccess() throws Exception {
@@ -66,7 +74,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 
 }
