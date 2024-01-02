83768d45e9d82214062333f8d13cc7d31fabfb63
w0nnie <kjw55523@gmail.com>
2024-01-02 12:09:36 +0900

    #20 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트 진행
    기본뷰 뼈대 작업
    테스트 media type 부가적인옵션 확인

--- src/main/java/com/guide/board/controller/ArticleController.java
+++ src/main/java/com/guide/board/controller/ArticleController.java
@@ -1,9 +1,18 @@
 package com.guide.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(Model model) {
+        String article = "";
+        model.addAttribute("article", article);
+        return "articles/index";
+    }
 }
--- src/test/java/com/guide/board/controller/ArticleControllerTest.java
+++ src/test/java/com/guide/board/controller/ArticleControllerTest.java
@@ -22,7 +22,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
+//    @Disabled("구현중")
     @DisplayName("[GET] 게시글 리스트 페이지 - 정상 호출")
     @Test
     void getArticleListViewSuccess() throws Exception {
@@ -31,7 +31,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("article"));
     }
