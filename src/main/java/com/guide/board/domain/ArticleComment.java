8fa0d6fa551b5adf918dfd2271993be3c72b212c
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-01-02 16:58:21 +0900

    계정 도메인 추가 작업 (#22)
    * #21 - 회원 계정 도메인 구현 및 erd 반영
    
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응
    
    * #21 - 리팩토링: 사용 안하는 import 정리
    
    import 정리

--- src/main/java/com/guide/board/domain/ArticleComment.java
+++ src/main/java/com/guide/board/domain/ArticleComment.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Objects;
 
