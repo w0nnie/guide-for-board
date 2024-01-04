b5926cf1c22ad7a0867dec49f84a58812f5e8c2c
w0nnie <kjw55523@gmail.com>
2024-01-04 14:39:59 +0900

    #20 - 시큐리티 config 모든 요청 열리도록 설정
    테스트 개발 단계

---
+++ src/main/java/com/guide/board/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.guide.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
